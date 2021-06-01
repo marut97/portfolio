30ae758aa877e796c35b376b62c663ed4274833d
marut97 <marut1997@gmail.com>
2021-06-01 23:40:28 +0530

    Folder structure correction

--- portfolio/src/resumeData.js
+++ portfolio/src/resumeData.js
@@ -1,106 +1,102 @@
 let resumeData = {
-  "imagebaseurl":"http://localhost:3000/",
-  "name": "John Doe",
-  "role": "Frontend Developer",
-  "linkedinId":"Your LinkedIn Id",
-  "skypeid": "Your skypeid",
-  "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
-  "socialLinks":[
-      {
-        "name":"linkedin",
-        "url":"https://www.linkedin.com/",
-        "className":"fa fa-linkedin"
-      },
-      {
-        "name":"github",
-        "url":"http://github.com/",
-        "className":"fa fa-github"
-      },
-      {
-        "name":"skype",
-        "url":"http://skype.com",
-        "className":"fa fa-skype"
-      }
-    ],
-  "aboutme":"I have finished my B.E. in Computer Science and have been working in industry for past few years.",
-  "address":"India",
-  "website":"https://www.codeclassifiers.com",
-  "education":[
+  imagebaseurl: "http://localhost:3000/",
+  name: "Marut Prasad",
+  role: "Full Stack Developer",
+  linkedinId: "Your LinkedIn Id",
+  roleDescription:
+    "I like dabbling in various parts of backend and frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+  socialLinks: [
     {
-      "UniversityName":"Mumbai University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
+      name: "linkedin",
+      url: "https:/www.linkedin.com/in/marut97",
+      className: "fa fa-linkedin",
     },
     {
-      "UniversityName":"Some University",
-      "specialization":"Some specialization",
-      "MonthOfPassing":"Jan",
-      "YearOfPassing":"2018",
-      "Achievements":"Some Achievements"
-    }
+      name: "github",
+      url: "https://github.com/marut97",
+      className: "fa fa-github",
+    },
   ],
-  "work":[
+  aboutme:
+    "I have finished my B.E. in Computer Science and have been working in industry for past few years.",
+  address: "India",
+  website: "https://www.codeclassifiers.com",
+  education: [
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
+      UniversityName: "Mumbai University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
     },
     {
-      "CompanyName":"Some Company",
-      "specialization":"Some specialization",
-      "MonthOfLeaving":"Jan",
-      "YearOfLeaving":"2018",
-      "Achievements":"Some Achievements"
-    }
+      UniversityName: "Some University",
+      specialization: "Some specialization",
+      MonthOfPassing: "Jan",
+      YearOfPassing: "2018",
+      Achievements: "Some Achievements",
+    },
   ],
-  "skillsDescription":"Your skills here",
-  "skills":[
+  work: [
     {
-      "skillname":"HTML5"
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
     },
     {
-      "skillname":"CSS"
+      CompanyName: "Some Company",
+      specialization: "Some specialization",
+      MonthOfLeaving: "Jan",
+      YearOfLeaving: "2018",
+      Achievements: "Some Achievements",
     },
-    {
-      "skillname":"Reactjs"
-    }
   ],
-  "portfolio":[
+  skillsDescription: "Your skills here",
+  skills: [
     {
-      "name":"project1",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
+      skillname: "HTML5",
     },
     {
-      "name":"project2",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project.jpg"
+      skillname: "CSS",
     },
     {
-      "name":"project3",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/project2.png"
+      skillname: "Reactjs",
     },
-    {
-      "name":"project4",
-      "description":"mobileapp",
-      "imgurl":"images/portfolio/phone.jpg"
-    }
   ],
-  "testimonials":[
+  portfolio: [
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
+      name: "project1",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
     },
     {
-      "description":"This is a sample testimonial",
-      "name":"Some technical guy"
-    }
-  ]
-}
+      name: "project2",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project.jpg",
+    },
+    {
+      name: "project3",
+      description: "mobileapp",
+      imgurl: "images/portfolio/project2.png",
+    },
+    {
+      name: "project4",
+      description: "mobileapp",
+      imgurl: "images/portfolio/phone.jpg",
+    },
+  ],
+  testimonials: [
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+    {
+      description: "This is a sample testimonial",
+      name: "Some technical guy",
+    },
+  ],
+};
 
-export default resumeData
+export default resumeData;
